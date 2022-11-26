7afab990c67f85bd012783ee00b08d97cb8712dc
Zammir Neftali Lopez Juarez <zammirnlj@gmail.com>
2022-11-26 11:15:15 -0600

    Controladores Asignados

---
+++ src/controllers/home.js
@@ -0,0 +1,16 @@
+const ctrl = {};
+
+ctrl. index = (req, res) => {
+    
+};
+
+ctrl. create = (req, res) => {
+   
+};
+
+ctrl. like = (req, res) => {
+   
+};
+
+
+module.exports = ctrl; 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,33 @@
-module.exports = app => {
+const express = require ('express');
+const router = express.Router();
 
-    app.get ('/', (req,res) =>{
+const home = require('../controllers/home');
+const image = require('../controllers/image');
+
+module.exports = app => {
+/*
+
+LO MISMO DE ABAJO PERO DEUCIDO E IMPORTADO 
+   router.get ('/', (req,res) =>{
 
         res.send('Index page');
     });
+*/
+
+router.get('/', home.index);
+
+//ruta para ver imagenes
+router.get('/images/:image_id', image.index);
+//ruta crear imagenes
+router.post('/images', image.create);
+//Ruta para dar like a la imagen
+router.post('/images/:image_id/like', image.like);
+//Comentarios en foto
+router.post('/images/:image_id/comment', image.comment);
+//Eliinar una imaagen
+router.delete('/images/:image_id', image.remove);
+
+app.use(router);
+
 
 };
